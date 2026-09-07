493198f06702b85e3d2ed9a581f4669f76e58d36
kushyme <159010501+kushyme@users.noreply.github.com>
2026-09-07 20:38:03 +0200

    [All hosts] - made brave the standard browser

--- home.nix
+++ home.nix
@@ -7,5 +7,24 @@
     home.homeDirectory = "/home/${hostVariables.username}";
     home.stateVersion = hostVariables.stateVersion;
     programs.home-manager.enable = true;
+
+    home.sessionVariables.BROWSER = "brave";
+
+    # GNOME's "Default Apps" cannot persist this: mimeapps.list is a
+    # read-only symlink into the store, so the default browser has to be
+    # declared here.
+    xdg.configFile."mimeapps.list".force = true;
+    xdg.dataFile."applications/mimeapps.list".force = true;
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = ["brave-browser.desktop"];
+        "x-scheme-handler/http" = ["brave-browser.desktop"];
+        "x-scheme-handler/https" = ["brave-browser.desktop"];
+        "x-scheme-handler/about" = ["brave-browser.desktop"];
+        "x-scheme-handler/unknown" = ["brave-browser.desktop"];
+      };
+    };
   };
 }
